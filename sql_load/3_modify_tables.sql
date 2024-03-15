da4d7ab88975260b21dd9630cf798b3daa70fcc6
Luke Barousse <luke.barousse@gmail.com>
2024-03-15 13:32:18 -0500

    fix modify table encoding error

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,8 +1,9 @@
 /* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
 Database Load Issues (follow if receiving permission denied when running SQL code below)
 
-NOTE: If you are having issues with permissions. And you get error: "could not open file
-"[your file path]\job_postings_fact.csv" for reading: Permission denied."
+NOTE: If you are having issues with permissions. And you get error: 
+
+'could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied.'
 
 1. Open pgAdmin
 2. In Object Explorer (left-hand pane), navigate to `sql_course` database
@@ -12,28 +13,29 @@ NOTE: If you are having issues with permissions. And you get error: "could not o
     1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
 5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
-\copy company_dim FROM '[insert file path]/csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+\copy company_dim FROM '[Insert File Path]/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_dim FROM '[insert file path]/csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+\copy skills_dim FROM '[Insert File Path]/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy job_postings_fact FROM '[insert file path]/csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+\copy job_postings_fact FROM '[Insert File Path]/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_job_dim FROM '[insert file path]/csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+\copy skills_job_dim FROM '[Insert File Path]/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 */
 
+-- NOTE: This has been updated from the video to fix issues with encoding
 COPY company_dim
-FROM '[insert file path]/csv_files/company_dim.csv'
-DELIMITER ',' CSV HEADER;
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM '[insert file path]/csv_files/skills_dim.csv'
-DELIMITER ',' CSV HEADER;
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM '[insert file path]/csv_files/job_postings_fact.csv'
-DELIMITER ',' CSV HEADER;
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM '[insert file path]/csv_files/skills_job_dim.csv'
-DELIMITER ',' CSV HEADER;
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
