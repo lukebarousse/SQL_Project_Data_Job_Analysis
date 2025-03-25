ff272240ac3d7952b9c512e308b213a8b2a263fe
Luke Barousse <luke.barousse@gmail.com>
2025-03-25 13:39:22 -0500

    update error troubleshooting message

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,17 +1,22 @@
 /* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
 Database Load Issues (follow if receiving permission denied when running SQL code below)
 
-NOTE: If you are having issues with permissions. And you get error: 
+Possible Errors: 
+- ERROR >>  duplicate key value violates unique constraint "company_dim_pkey"
+- ERROR >> could not open file "C:\Users\...\company_dim.csv" for reading: Permission denied
 
-'could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied.'
-
-1. Open pgAdmin
-2. In Object Explorer (left-hand pane), navigate to `sql_course` database
-3. Right-click `sql_course` and select `PSQL Tool`
-    - This opens a terminal window to write the following code
-4. Get the absolute file path of your csv files
-    1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
-5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
+1. Drop the Database 
+            DROP DATABASE IF EXISTS sql_course;
+2. Repeat steps to create database and load table schemas
+            - 1_create_database.sql
+            - 2_create_tables.sql
+3. Open pgAdmin
+4. In Object Explorer (left-hand pane), navigate to `sql_course` database
+5. Right-click `sql_course` and select `PSQL Tool`
+            - This opens a terminal window to write the following code
+6. Get the absolute file path of your csv files
+            1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
+7. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
 \copy company_dim FROM '[Insert File Path]/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
